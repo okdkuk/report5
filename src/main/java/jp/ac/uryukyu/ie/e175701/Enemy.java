d7f258a00b81f0fb027e6ee1c46f929353a193ea
Koki Okuda <e175701@ie.u-ryukyu.ac.jp>
2017-12-11 17:51:10 +0900

    1st

---
+++ src/main/java/jp/ac/uryukyu/ie/e175701/Enemy.java
@@ -0,0 +1,16 @@
+package jp.ac.uryukyu.ie.e175701;
+
+public class Enemy extends LivingThing {
+
+    public Enemy(String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+
+    public void wounded(int damage){
+        setHitPoint(getHitPoint()-damage);
+        if( getHitPoint() < 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
+    }
+}
